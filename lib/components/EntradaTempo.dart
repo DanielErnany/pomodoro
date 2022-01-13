6bca40c07d7d67ae690e21be9acd5d48b4e6ad99
Daniel Ernany <ddernany@gmail.com>
2022-01-13 10:25:59 -0300

    feat: alterar tempo definido para cada tarefa através dos botões do widget EntradaTempo

--- lib/components/EntradaTempo.dart
+++ lib/components/EntradaTempo.dart
@@ -4,10 +4,15 @@ class EntradaTempo extends StatelessWidget {
   final String titulo;
   final int valor;
 
+  final void Function()? inc;
+  final void Function()? dec;
+
   const EntradaTempo({
     Key? key,
     required this.titulo,
     required this.valor,
+    this.inc,
+    this.dec,
   }) : super(key: key);
 
   @override
@@ -28,7 +33,7 @@ class EntradaTempo extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {},
+              onPressed: this.dec,
               child: Icon(
                 Icons.arrow_downward,
                 color: Colors.white,
@@ -46,7 +51,7 @@ class EntradaTempo extends StatelessWidget {
               ),
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: this.inc,
               child: Icon(
                 Icons.arrow_upward,
                 color: Colors.white,
