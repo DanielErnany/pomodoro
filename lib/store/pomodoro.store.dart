adddddfd6f788e769c3f7b9782c5eed9f01acbdf
Daniel Ernany <ddernany@gmail.com>
2022-01-13 12:07:22 -0300

    fix: corrigido os métodos para diminuir o tempo das tarefas para que ele não seja negativo ou nulo

--- lib/store/pomodoro.store.dart
+++ lib/store/pomodoro.store.dart
@@ -77,9 +77,11 @@ abstract class _PomodoroStore with Store {
 
   @action
   void decrementarTempoTrabalho() {
-    tempoTrabalho--;
-    if (estaTrabalhando()) {
-      reiniciar();
+    if (tempoTrabalho > 1) {
+      tempoTrabalho--;
+      if (estaTrabalhando()) {
+        reiniciar();
+      }
     }
   }
 
@@ -93,9 +95,11 @@ abstract class _PomodoroStore with Store {
 
   @action
   void decrementarTempoDescanso() {
-    tempoDescanso--;
-    if (estaDescansando()) {
-      reiniciar();
+    if (tempoDescanso > 1) {
+      tempoDescanso--;
+      if (estaDescansando()) {
+        reiniciar();
+      }
     }
   }
 
