f60ece60c567df5a730d76f87e7a79d863b6f32b
Daniel Ernany <ddernany@gmail.com>
2022-01-13 08:40:43 -0300

    feat: widget Cronometro
    Foi criado o widget responsavél por mostrar o tempo decorrido.

--- lib/pages/Pomodoro.dart
+++ lib/pages/Pomodoro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/components/Cronometro.dart';
 import 'package:pomodoro/components/EntradaTempo.dart';
 
 class Pomodoro extends StatelessWidget {
@@ -6,14 +7,20 @@ class Pomodoro extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('Pomodoro'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              EntradaTempo(titulo: 'Trabalho', valor: 30),
-              EntradaTempo(titulo: 'Descanso', valor: 30),
-            ],
+          Expanded(
+            child: Cronometro(),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                EntradaTempo(titulo: 'Trabalho', valor: 30),
+                EntradaTempo(titulo: 'Descanso', valor: 30),
+              ],
+            ),
           ),
         ],
       ),
